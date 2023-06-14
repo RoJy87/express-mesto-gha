c43c40c546e16a9736061946a50d1cf3010fd1ac
Danil Simonov <simdanila@gmailcom>
2023-06-15 00:40:03 +0300

    Исправление ошибок теста

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,12 +2,11 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  const { token } = req.cookies;
+  if (!token) {
     return res.status(401).send({ message: 'Необходима авторизация' });
   }
 
-  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
