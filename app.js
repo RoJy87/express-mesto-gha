2132de44c5ec52b26535c3de54213159b3ac2684
Danil Simonov <simdanila@gmailcom>
2023-06-30 23:50:11 +0300

    refactor cards

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const helmet = require('helmet');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-// const cors = require('cors');
-const cors = require('./middlewares/cors');
+const cors = require('cors');
+// const cors = require('./middlewares/cors');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./middlewares/errors/NotFoundError');
 const customErrors = require('./middlewares/errors/customErrors');
@@ -22,24 +22,24 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 1000,
 });
-
-// app.use(cors({
-//   origin: [
-//     'http://localhost:3000',
-//     'https://api.simon.mesto.nomoreparties.sbs',
-//     'https://simon.mesto.nomoreparties.sbs',
-//   ],
-//   credentials: true,
-//   methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH'],
-// }));
 
 app.use(limiter);
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors);
+// app.use(cors);
+
+app.use(cors({
+  origin: [
+    'http://localhost:3001',
+    'https://api.simon.mesto.nomoreparties.sbs',
+    'https://simon.mesto.nomoreparties.sbs',
+  ],
+  credentials: true,
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH'],
+}));
 
 app.use(requestLogger);
 
